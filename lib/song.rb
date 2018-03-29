5b26a592108b67d1e16f705ef25226d2875518fc
Marin Cabac <marin.cabac@gmail.com>
2018-03-29 20:34:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,22 +1,18 @@
 require 'pry'
 
 class Song
-
 attr_reader :name, :artist
-
 
   def initialize(name)
     @name = name
   end
-
-  def name=(name)
-    @name = name
-  end
+   
 
 def artist(name)
   self.artist = Artist.find_or_create_by_name(name)
   artist.add_song(self)
 end
+###############################################
 
 def self.new_by_filename(file)
   song = self.new
