4dd9df267e45e7579c2d59ce3356f3e96d65514b
Marin Cabac <marin.cabac@gmail.com>
2018-03-29 20:36:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ attr_reader :name, :artist
 
     def artist_name=(name)
         self.artist = Artist.find_or_create_by_name(name)
-        
+
         artist.add_song(self)
       end
 ###############################################
